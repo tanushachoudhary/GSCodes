eb8750634cf4aaaddd92073d4229e72fefa3a458
Teesha Dembla <teeshadembla0507@gmail.com>
2025-02-13 15:28:39 +0530

    Login page with browser router

---
+++ frontend/gscode/src/Pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/Navbar"
+import Waves from "../components/Waves"
+
+export default function Login() {
+
+  return (
+    
+    <div className="flex flex-col justify-between min-h-screen bg-gray-100">
+      <div className="flex-grow flex items-center justify-center">
+        <Navbar />
+      </div>
+
+      <div className="w-full fixed bottom-0">
+        <Waves />
+      </div>
+    </div>
+  );
+}
