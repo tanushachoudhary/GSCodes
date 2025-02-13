09a9cfad80a41760a4cee23228f1213984d6f145
yashseth1207 <yashseth20030712@gmail.com>
2025-02-13 16:04:56 +0530

    new:footer.jsx

--- frontend/gscode/src/App.jsx
+++ frontend/gscode/src/App.jsx
@@ -3,17 +3,19 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Waves from "./components/Waves";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <div className="flex-grow flex justify-center">
+      {/* <div className="flex-grow flex justify-center">
         <Navbar />
       </div>
 
       <div className="w-full fixed bottom-0">
         <Waves />
-      </div>
+      </div> */}
+      <Footer></Footer>
 
 
     </div>
