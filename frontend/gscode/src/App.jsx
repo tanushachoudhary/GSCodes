3604a4bc90571a4bd8fb957c6b6b6db399d3a196
yashseth1207 <yashseth20030712@gmail.com>
2025-02-15 20:47:58 +0530

    fix:footer completely working

--- frontend/gscode/src/App.jsx
+++ frontend/gscode/src/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
+import Footer from "./components/Footer"
 import "./App.css";
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login/>} />
+      <Route path="/" element={<Footer/>} />
 
       <Route path="/profile" element={<Profile/>} />
     </Routes>
