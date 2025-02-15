046dafd33af46c63699a6a6ec89dc96cd6f5e7aa
Teesha Dembla <teeshadembla0507@gmail.com>
2025-02-16 02:44:14 +0530

    Heat map on profile page done

--- frontend/gscode/src/App.jsx
+++ frontend/gscode/src/App.jsx
@@ -1,5 +1,4 @@
 // App.js
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
--- frontend/gscode/src/Pages/Profile.jsx
+++ frontend/gscode/src/Pages/Profile.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import HeatMap from "../components/HeatMap";
+import React, { useEffect, useState } from "react";
+import HeatMapData from "../assets/Data/HeatmapStatic";
+import HeatMapBar from "../components/ProfilePage/HeatMap/HeatMapBar";
+
 const dummyUserData = {
     username:"",
     name:"",
@@ -7,14 +9,18 @@ const dummyUserData = {
 }
 
 function Profile(){
+    let [submissions, setSubmissions] = useState(0);
+
+    useEffect(()=>{
+        setSubmissions(HeatMapData.reduce((acc, day)=> acc+day.commits, 0));
+    },[]);
+
     return(
-        <div className="flex flex-col w-screen h-screen justify-center items-center bg-blue-950">
-            <div className="flex flex-col w-5xl h-screen justify-center items-center border border-white">
-                <div className="flexflex-col">
-                    <div id="userImg" className="bg-black"></div>
-                </div>
-                <div className="flex justify-center items-center bg-indigo-950">
-                    <HeatMap/>
+        <div className="flex flex-col w-screen h-screen justify-center items-center bg-indigo-950">
+            <div className="flex flex-col w-5xl h-screen justify-center items-center p-10">
+                <div className="flex flex-col justify-center items-center py-5 bg-blue-950">
+                    <h1 className="text-2xl font-medium ml-5">{submissions} submissions in last 6 months</h1>
+                    <HeatMapBar/>
                 </div>
             </div>            
         </div>
