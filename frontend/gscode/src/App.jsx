954b69613f86145853c7253b97477679ef170525
yashseth1207 <yashseth20030712@gmail.com>
2025-02-16 13:41:55 +0530

    added:Community Page

--- frontend/gscode/src/App.jsx
+++ frontend/gscode/src/App.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Community from "./components/Community";
 import "./App.css";
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>} />
-
       <Route path="/profile" element={<Profile/>} />
+      <Route path="/community" element={<Community />} />
     </Routes>
    </BrowserRouter>
   )
